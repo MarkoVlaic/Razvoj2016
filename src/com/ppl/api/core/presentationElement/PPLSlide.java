0de989e76fc5236c1c41d9d2cc7089cf4cc88b35
Marko Vlaic <vlaic.marko@gmail.com>
2016-10-18 19:19:24 +0200

    Changed the PPLPresentation class

--- src/com/ppl/api/core/presentationElement/PPLSlide.java
+++ src/com/ppl/api/core/presentationElement/PPLSlide.java
@@ -15,10 +15,8 @@ public class PPLSlide extends PPLPresentationElement {
     @Override
     public void addChild(PPLPresentationElement child)
     {
-        if(child.getClass() == PPLSlide.class || child.getClass() == PPLPresentation.class){
+        if(child.getClass() == PPLSlide.class){
             try {
-                throw new InvalidArgumentException(new String[]{"Cannot add a child of type " + child.getClass() + " to slide directly"});
-            } catch (InvalidArgumentException e) {
                 e.printStackTrace();
             }
         }else{
