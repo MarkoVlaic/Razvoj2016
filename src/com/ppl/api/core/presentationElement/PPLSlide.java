731e9c15bfa0885ad10a956c454d324fc344d7fa
Marko Vlaic <vlaic.marko@gmail.com>
2016-10-18 19:23:03 +0200

    Done

--- src/com/ppl/api/core/presentationElement/PPLSlide.java
+++ src/com/ppl/api/core/presentationElement/PPLSlide.java
@@ -1,6 +1,7 @@
 package com.ppl.api.core.presentationElement;
 
 import com.sun.javaws.exceptions.InvalidArgumentException;
+import jdk.nashorn.internal.runtime.ECMAException;
 
 import java.io.InvalidClassException;
 import java.util.LinkedList;
@@ -17,6 +18,8 @@ public class PPLSlide extends PPLPresentationElement {
     {
         if(child.getClass() == PPLSlide.class){
             try {
+                throw new Exception("PPLSlide cannot be a child of a PPLSlide class");
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }else{
